e81229419fd05a2b434dbb4fd6b94e8668641da8
Jerry Craig <cupbearer@foodnav.org>
2019-07-04 00:20:35 -0700

    initial setup 3

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    
+    <div class="row">
+      <div class="col-3 p-5">
+        <img src="https://instagram.fphx1-1.fna.fbcdn.net/vp/a2729ba61f6bf294969f75d3631a6adc/5DB47138/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fphx1-1.fna.fbcdn.net" class="rounded-circle">
+      </div>
+      <div class="col-9 pt-5">
+        <div><h1>freecodecamp</h1></div>
+          <div class="d-flex">
+            <div class="pr-5"><strong>153</strong> posts</div>
+            <div class="pr-5"><strong>22k</strong> followers</div>
+            <div class="pr-5"><strong>212</strong> following</div>
+          </div>
+      </div>
+    </div>
+    
+</div>
+@endsection
